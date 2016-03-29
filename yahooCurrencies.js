ee639aff5b7cc6829d9b8844acffa0e5966ae0a5
Wilson Ndirangu <wndirangu@cytonn.com>
2016-03-29 17:20:38 +0300

    changes

--- yahooCurrencies.js
+++ yahooCurrencies.js
@@ -6,28 +6,56 @@
 
 // get  all the currencies and their current status
 var http = require("http");
+var fs = require("fs");
+
+
+fs.readFile('Index.html', function(err, html) {
+  if (err) {
+    throw err;
+  }
+  http.createServer(function(request, res) {
+    res.writeHeader(200, {
+      "Content-Type": "text/html"
+    });
+    res.write(html);
+
+
     url = "http://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json";
 
-// get is a simple wrapper for request()
-// which sets the http method to GET
-var request = http.get(url, function (response) {
-    // data is streamed in chunks from the server
-    // so we have to handle the "data" event
-    var buffer = "",
+
+    // get is a simple wrapper for request()
+    // which sets the http method to GET
+
+    request = http.get(url, function(response) {
+      // data is streamed in chunks from the server
+      // so we have to handle the "data" event
+      var buffer = "",
         data,
         Quotes;
 
-    response.on("data", function (chunk) {
+      response.on("data", function(chunk) {
         buffer += chunk;
-    });
 
-    response.on("end", function (err) {
+      });
+
+      response.on("end", function(err) {
         // finished transferring data dump the raw data
 
         data = JSON.parse(buffer);
         Quotes = data.list.resources;
-        for (i=0; i<data.list.resources.length; i++ ){
-    console.log((Quotes[i].resource.fields.symbol));
-}
+        //data.list.resources.length
+
+
+        for (i = 0; i < 10; i++) {
+
+          console.log((Quotes[i].resource.fields));
+
+        }
+
+      });
     });
+
+    res.end();
+  }).listen(3000); // Activates this server, listening on port 8080.
+
 });
